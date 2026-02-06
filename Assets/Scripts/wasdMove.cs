1368007214c2c73cb33f14eab57daf5e4a120596
Ian Hughes <ianhug88@gmail.com>
2026-02-06 12:55:30 -0500

    named new scene and added some to wasdMove

--- Assets/Scripts/wasdMove.cs
+++ Assets/Scripts/wasdMove.cs
@@ -1,7 +1,14 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class wasdMove : MonoBehaviour
 {
+    public float moveSpeed;
+    bool positiveX;
+    bool positiveZ;
+    bool negativeX;
+    bool negativeZ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,14 @@ public class wasdMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (keypressed w == true)
+        {
+            positiveX = true;
+        } else { positiveX = false; };
+
+        if keypressed s  == true){
+            negativeX = true;
+        } else { }
+            gameObject.transform
     }
 }
