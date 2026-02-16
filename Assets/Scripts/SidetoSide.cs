eab8711226b1dfe95faa9acb5a2574d28c3f53ca
Anthony Varela <anthonyvarelajimenez741@gmail.com>
2026-02-15 23:18:27 -0500

    Update script.

--- Assets/Scripts/SidetoSide.cs
+++ Assets/Scripts/SidetoSide.cs
@@ -2,21 +2,20 @@ using UnityEngine;
 
 public class SidetoSide : MonoBehaviour{
 
-    public float speed = 2.0f;
-    public float distance = 3f;
-    private Vector3 startPos;
+    public float speed = 2.0f; //the speed we want the object to move
+    public float distance = 3f; //the distance we wnat it to travel from 
+    private Vector3 startPos; //the starting position
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        startPos = transform.position;
-
+        startPos = transform.position; //the start position is the current position of the object
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement = Mathf.PingPong(Time.time * speed, distance);
-        transform.position = startPos + new Vector3 (0, 0, movement);
+        float movement = Mathf.PingPong(Time.time * speed, distance); //updates every second until it reaches distance and goes back, ping pong method. 
+        transform.position = startPos + new Vector3 (0, 0, movement); //update the position of the transfrom by adding the current position and moving the z axis
     }
 }
