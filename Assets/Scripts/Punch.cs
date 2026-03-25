9cd670339ada334600fd9c1e9be979cbcc094fac
Anthony Varela <anthonyvarelajimenez741@gmail.com>
2026-03-24 23:33:38 -0400

    Added punching hammer.

---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+
+    public float speed = 2.0f; //the speed we want the object to move
+    public float distance = 3f; //the distance we wnat it to travel from 
+    private Vector3 startPos; //the starting position
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position; //the start position is the current position of the object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Mathf.PingPong(Time.time * speed, distance); //updates every second until it reaches distance and goes back, ping pong method. 
+        transform.position = startPos + new Vector3(movement, 0, 0); //update the position of the transfrom by adding the current position and moving the z axis
+    }
+}
